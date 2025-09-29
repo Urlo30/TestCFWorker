323915579aa57d289b6a58436787569b0433771f
Urlo30 <91965455+UrloMythus@users.noreply.github.com>
2025-09-29 07:43:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import requests
-response = requests.get('https://floral.urlo30.workers.dev')
+headers = {
+  "Accept" : "*/*",
+  "Alt-Used" : "vixsrc.to",
+  "Connection" : "keep-alive",
+  "Host": "vixsrc.to",
+  "Referer" : "https://vixsrc.to/",
+  "Sec-Fetch-Dest" : "iframe",
+  "Sec-Fetch-Mode" : "navigate",
+  "Sec-Fetch-Site" : "cross-site",
+  "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/133.0",
+}
+response = requests.get('https://vixsrc.to/movie/786892', headers = headers)
 print(response.text)
+print(response)
